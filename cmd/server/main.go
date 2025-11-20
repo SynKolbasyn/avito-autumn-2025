75189fe64fd50b20d3b27dc61dfbf995ac035974
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2025-11-20 03:30:20 +0300

    feat: created web service using `InitLogger`, `SetupMiddleware`, `RegisterRoutes` and `StartServer`

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,4 +1,23 @@
 // Package main starts the web service.
 package main
 
-func main() {}
+import (
+	"autumn-2025/internal/server"
+	"autumn-2025/pkg/logger"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	logger.InitLogger()
+
+	e := echo.New()
+	e.HideBanner = true
+	e.HidePort = true
+
+	server.SetupMiddleware(e)
+	server.RegisterRoutes(e)
+	server.StartServer(e)
+}
+
+
