90a9652e8f67bfaf853abe2dbf945e803eb170d0
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2025-11-20 03:23:11 +0300

    feat: added logger initializetion function `InitLogger`

---
+++ pkg/logger/logger.go
@@ -0,0 +1,21 @@
+package logger
+
+import (
+	"os"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func InitLogger() {
+	zerolog.TimeFieldFormat = time.RFC3339
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout})
+
+	env := os.Getenv("ENV")
+	if env == "production" {
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	} else {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+}
