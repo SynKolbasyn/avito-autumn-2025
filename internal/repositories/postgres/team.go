c08d83459cd8791430cda8faa6d5b0b0ad70ee44
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2025-11-23 12:15:38 +0300

    feat: added зщыеpostgres repository team implementation

--- internal/repositories/postgres/team.go
+++ internal/repositories/postgres/team.go
@@ -1,6 +1,13 @@
 package postgres
 
-import "github.com/jackc/pgx/v5/pgxpool"
+import (
+	"autumn-2025/internal/repositories"
+	"context"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/rs/zerolog/log"
+)
 
 type TeamRepository struct {
 	pool *pgxpool.Pool
@@ -8,4 +15,45 @@ type TeamRepository struct {
 
 func NewTeamRepository(pool *pgxpool.Pool) *TeamRepository {
 	return &TeamRepository{pool}
+}
+
+func (t *TeamRepository) WithTransaction(ctx context.Context, f func(ctx context.Context) error) error {
+	tx, err := t.pool.Begin(ctx)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := tx.Rollback(ctx); err != nil {
+			log.Error().Err(err).Msg("failed to rollback transaction")
+		}
+	}()
+
+	if err := f(context.WithValue(ctx, repositories.TxContextKey, tx)); err != nil {
+		return err
+	}
+
+	return tx.Commit(ctx)
+}
+
+func (t *TeamRepository) GetExecutor(ctx context.Context) repositories.Executor {
+	if tx, ok := ctx.Value(repositories.TxContextKey).(pgx.Tx); ok {
+		return tx
+	}
+	return t.pool
+}
+
+func (t *TeamRepository) CreateTeam(ctx context.Context, teamName string) error {
+	query := `
+	INSERT INTO teams (name) VALUES ($1)
+	ON CONFLICT (name) DO NOTHING;
+	`
+	executor := t.GetExecutor(ctx)
+	tag, err := executor.Exec(ctx, query, teamName)
+	if err != nil {
+		return err
+	}
+	if tag.RowsAffected() != 1 {
+		return repositories.TeamAlreadyExistsError
+	}
+	return nil
 }
