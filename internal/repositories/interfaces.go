cd1c4cbbcc98578034a4fc8d5153808ebaca7d48
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2025-11-23 12:13:28 +0300

    feat: added 2 additional interfaces to repositories: `TransactionManager` and `Executor`

--- internal/repositories/interfaces.go
+++ internal/repositories/interfaces.go
@@ -1,4 +1,28 @@
 package repositories
 
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+)
+
+type repositoryContextKey string
+
+const TxContextKey repositoryContextKey = "pgx_tx"
+
+type Executor interface {
+	Exec(ctx context.Context, sql string, arguments ...any) (commandTag pgconn.CommandTag, err error)
+	Query(ctx context.Context, sql string, args ...any) (pgx.Rows, error)
+	QueryRow(ctx context.Context, sql string, args ...any) pgx.Row
+}
+
+type TransactionManager interface {
+	WithTransaction(ctx context.Context, f func(ctx context.Context) error) error
+	GetExecutor(ctx context.Context) Executor
+}
+
 type TeamRepository interface {
+	TransactionManager
+	CreateTeam(ctx context.Context, teamName string) error
 }
