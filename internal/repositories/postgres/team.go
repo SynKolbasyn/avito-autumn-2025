10b27983a92f88cde2bd4d9b48bda6283df33995
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2025-11-23 15:34:05 +0300

    feat: added postgres `TeamRepository` methods

--- internal/repositories/postgres/team.go
+++ internal/repositories/postgres/team.go
@@ -1,9 +1,13 @@
 package postgres
 
 import (
+	"autumn-2025/internal/models/dto"
 	"autumn-2025/internal/repositories"
 	"context"
+	"errors"
+	"fmt"
 
+	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/rs/zerolog/log"
@@ -42,18 +46,69 @@ func (t *TeamRepository) GetExecutor(ctx context.Context) repositories.Executor 
 	return t.pool
 }
 
-func (t *TeamRepository) CreateTeam(ctx context.Context, teamName string) error {
+func (t *TeamRepository) CreateTeam(ctx context.Context, teamName string) (uuid.UUID, error) {
 	query := `
 	INSERT INTO teams (name) VALUES ($1)
-	ON CONFLICT (name) DO NOTHING;
+	ON CONFLICT (name) DO NOTHING
+	RETURNING id;
 	`
 	executor := t.GetExecutor(ctx)
-	tag, err := executor.Exec(ctx, query, teamName)
+	row := executor.QueryRow(ctx, query, teamName)
+	var id uuid.UUID
+	err := row.Scan(&id)
+	if errors.Is(err, pgx.ErrNoRows) {
+		return uuid.UUID{}, repositories.TeamAlreadyExistsError
+	}
 	if err != nil {
-		return err
+		return uuid.UUID{}, err
 	}
-	if tag.RowsAffected() != 1 {
-		return repositories.TeamAlreadyExistsError
+	return id, nil
+}
+
+func (t *TeamRepository) InsertOrUpdateUsers(ctx context.Context, teamMembers []dto.TeamMember) ([]uuid.UUID, error) {
+	var (
+		valuesCount = 3
+		query       = "INSERT INTO users (id, name, is_active) VALUES "
+		params      = make([]interface{}, 0, len(teamMembers)*valuesCount)
+		ids         = make([]uuid.UUID, 0, len(teamMembers))
+	)
+	for i, user := range teamMembers {
+		if i > 0 {
+			query += ", "
+		}
+		query += fmt.Sprintf("($%d, $%d, $%d)", valuesCount*i+1, valuesCount*i+2, valuesCount*i+3)
+		params = append(params, user.UserID, user.Username, user.IsActive)
+		ids = append(ids, user.UserID)
 	}
-	return nil
+	query += `
+    ON CONFLICT (id) DO UPDATE SET
+		name = EXCLUDED.name,
+		is_active = EXCLUDED.is_active,
+		updated_at = CURRENT_TIMESTAMP;
+    `
+	executor := t.GetExecutor(ctx)
+	_, err := executor.Exec(ctx, query, params...)
+	if err != nil {
+		return nil, err
+	}
+	return ids, err
+}
+
+func (t *TeamRepository) AddTeamMembers(ctx context.Context, teamID uuid.UUID, memberIDS []uuid.UUID) error {
+	var (
+		valuesCount = 2
+		query       = "INSERT INTO user_teams (user_id, team_id) VALUES "
+		params      = make([]interface{}, 0, len(memberIDS)*valuesCount)
+	)
+	for i, memberID := range memberIDS {
+		if i > 0 {
+			query += ", "
+		}
+		query += fmt.Sprintf("($%d, $%d)", valuesCount*i+1, valuesCount*i+2)
+		params = append(params, memberID, teamID)
+	}
+	query += "ON CONFLICT (user_id, team_id) DO NOTHING;"
+	executor := t.GetExecutor(ctx)
+	_, err := executor.Exec(ctx, query, params...)
+	return err
 }
