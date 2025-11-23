83705e547f3cdb0e2ccc33fe296098ac95d622de
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2025-11-23 18:29:23 +0300

    feat: added pull request dto

---
+++ internal/models/dto/pull_request.go
@@ -0,0 +1,22 @@
+package dto
+
+import "github.com/google/uuid"
+
+type PullRequestStatus string
+
+const (
+	PullRequestStatusOpen   PullRequestStatus = "OPEN"
+	PullRequestStatusClosed PullRequestStatus = "CLOSED"
+)
+
+type PullRequestCreate struct {
+	PullRequestID   uuid.UUID `json:"pull_request_id"`
+	PullRequestName string    `json:"pull_request_name"`
+	AuthorID        uuid.UUID `json:"author_id"`
+}
+
+type PullRequestCreated struct {
+	PullRequestCreate
+	Status            PullRequestStatus `json:"status"`
+	AssignedReviewers []uuid.UUID       `json:"assigned_reviewers"`
+}
