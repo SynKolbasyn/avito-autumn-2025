c90f32bf4b3eff3ee54c4b2f12c5ba4516b7b01a
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2025-11-23 15:36:12 +0300

    feat: added `TeamHandler.Add` method

--- internal/handlers/team.go
+++ internal/handlers/team.go
@@ -1,25 +1,46 @@
 package handlers
 
 import (
+	"autumn-2025/internal/models/dto"
 	"autumn-2025/internal/repositories"
 	"autumn-2025/internal/services"
+	"context"
+	"errors"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/rs/zerolog/log"
 )
 
 type TeamHandler struct {
+	ctx         context.Context
 	teamService *services.TeamService
 }
 
-func NewTeamHandler(repository repositories.TeamRepository) *TeamHandler {
+func NewTeamHandler(ctx context.Context, repository repositories.TeamRepository) *TeamHandler {
 	return &TeamHandler{
+		ctx:         ctx,
 		teamService: services.NewTeamService(repository),
 	}
 }
 
 func (handler *TeamHandler) Add(c echo.Context) error {
-	return c.JSON(http.StatusNotImplemented, "not implemented")
+	var team dto.Team
+	if err := c.Bind(&team); err != nil {
+		log.Info().Err(err).Msg("StatusBadRequest")
+		return c.JSON(http.StatusBadRequest, err)
+	}
+
+	team, err := handler.teamService.CreateTeam(handler.ctx, team)
+	if err != nil {
+		log.Info().Err(err).Msg("teamService.CreateTeam failed")
+		if errors.Is(err, repositories.TeamAlreadyExistsError) {
+			return c.JSON(http.StatusConflict, dto.TeamAlreadyExists(team.TeamName))
+		}
+		return c.JSON(http.StatusBadRequest, "Something went wrong")
+	}
+
+	return c.JSON(http.StatusCreated, map[string]dto.Team{"team": team})
 }
 
 func (handler *TeamHandler) Get(c echo.Context) error {
