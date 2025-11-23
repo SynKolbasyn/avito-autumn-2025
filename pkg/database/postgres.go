c2665aa801ae8713b81ce500b1c98f2976f6ac89
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2025-11-23 10:18:02 +0300

    feat: added database pool connection

---
+++ pkg/database/postgres.go
@@ -0,0 +1,17 @@
+package database
+
+import (
+	"autumn-2025/config"
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/rs/zerolog/log"
+)
+
+func Pool(ctx context.Context, cfg *config.Config) *pgxpool.Pool {
+	pool, err := pgxpool.New(ctx, cfg.ServerAddress())
+	if err != nil {
+		log.Fatal().Err(err).Str("server_address", cfg.ServerAddress()).Msg("failed to create database pool")
+	}
+	return pool
+}
