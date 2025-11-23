2b8abeab6c504f94204b640b72466b2b027d8f70
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2025-11-23 10:51:23 +0300

    feat: added DTO models `TeamMember` and `Team`

---
+++ internal/models/dto/user.go
@@ -0,0 +1,9 @@
+package dto
+
+import "github.com/google/uuid"
+
+type TeamMember struct {
+	UserID   uuid.UUID `json:"user_id"`
+	Username string    `json:"username"`
+	IsActive bool      `json:"is_active"`
+}
