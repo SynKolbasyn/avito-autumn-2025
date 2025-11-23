a77e72e0114f378e1e099606dc6dedcae5012798
Andrew Kozmin <syn.kolbasyn.06@gmail.com>
2025-11-23 15:35:20 +0300

    feat: added `TeamService.CreateTeam` method

--- internal/services/team.go
+++ internal/services/team.go
@@ -1,6 +1,10 @@
 package services
 
-import "autumn-2025/internal/repositories"
+import (
+	"autumn-2025/internal/models/dto"
+	"autumn-2025/internal/repositories"
+	"context"
+)
 
 type TeamService struct {
 	teamRepository repositories.TeamRepository
@@ -10,4 +14,26 @@ func NewTeamService(repository repositories.TeamRepository) *TeamService {
 	return &TeamService{
 		teamRepository: repository,
 	}
+}
+
+func (t *TeamService) CreateTeam(ctx context.Context, team dto.Team) (dto.Team, error) {
+	err := t.teamRepository.WithTransaction(ctx, func(c context.Context) error {
+		id, err := t.teamRepository.CreateTeam(c, team.TeamName)
+		if err != nil {
+			return err
+		}
+
+		ids, err := t.teamRepository.InsertOrUpdateUsers(c, team.Members)
+		if err != nil {
+			return err
+		}
+
+		return t.teamRepository.AddTeamMembers(c, id, ids)
+	})
+
+	if err != nil {
+		return dto.Team{}, err
+	}
+
+	return team, nil
 }
